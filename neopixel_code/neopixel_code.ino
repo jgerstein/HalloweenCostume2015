c7e96a1a4124878dc6b632f7bdf615c261b7e824
jgerstein <gersteinj@gmail.com>
2015-10-30 07:27:30 -0400

    set up test code

--- neopixel_code/neopixel_code.ino
+++ neopixel_code/neopixel_code.ino
@@ -2,7 +2,7 @@
 #include<FastLED.h>
 
 //define the number of LEDs being used
-#define NUM_LEDS 89
+#define NUM_LEDS 57
 
 //declare an array of LEDs
 CRGB leds[NUM_LEDS];
@@ -12,7 +12,7 @@ float hues[NUM_LEDS];
 float bright[NUM_LEDS];
 float change[NUM_LEDS];
 
-int hue = 30;
+int hue;
 
 int waitTime = 10;
 
@@ -20,23 +20,23 @@ void setup() {
   delay(3000);
   //add the LEDs and set brightness
   LEDS.addLeds<WS2812B, 5, GRB>(leds, NUM_LEDS);
-  LEDS.setBrightness(80);
+  LEDS.setBrightness(30);
 
   for (int i = 0; i < NUM_LEDS; i++) {
-    hues[i] = random(255);
-    bright[i] = random(50,200);
-    change[i] = random(-1,1);
+    hues[i] = map(i, 0, NUM_LEDS, 0, 255);
+    bright[i] = 255;
+    //    change[i] = random(-1,1);
   }
 }
 
 void loop() {
   //go through the entire array and assign a color to each LED
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CHSV(hues[i], 0, bright[i]);
-    bright[i] += change[i];
-    if (bright[i] > 250 || bright[i] < 5) {
-      change[i] *= -1;
-    }
+    leds[i] = CHSV(hues[i], 255, bright[i]);
+    //    bright[i] += change[i];
+    //    if (bright[i] > 250 || bright[i] < 5) {
+    //      change[i] *= -1;
+    //    }
   }
 
 
